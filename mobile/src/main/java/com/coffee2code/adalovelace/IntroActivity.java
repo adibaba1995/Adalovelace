53cb12d71af1e29b3a735c9bad0c7021463ca0fb
adityathanekar <aditya.thanekar@gmail.com>
2017-01-08 15:02:45 +0530

    "Yo aditya"

--- mobile/src/main/java/com/coffee2code/adalovelace/IntroActivity.java
+++ mobile/src/main/java/com/coffee2code/adalovelace/IntroActivity.java
@@ -1,9 +1,9 @@
 package com.coffee2code.adalovelace;
 
-import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -19,6 +19,9 @@ public class IntroActivity extends AppIntro2 {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Fragment firstFragment = new UserInfoFragment();
         addSlide(AppIntroFragment.newInstance("THIS IS TEST  ", "HELLLO SOME SMALL DESCRIPTION", R.mipmap.ic_launcher, getResources().getColor(R.color.colorAccent)));
+        addSlide(firstFragment);
     }
 }
--- mobile/src/main/java/com/coffee2code/adalovelace/UserInfoFragment.java
+++ mobile/src/main/java/com/coffee2code/adalovelace/UserInfoFragment.java
@@ -1,13 +1,16 @@
 package com.coffee2code.adalovelace;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 /**
  * Created by adityathanekar on 08/01/17.
@@ -17,14 +20,34 @@ public class UserInfoFragment extends Fragment {
 
     private AppCompatSpinner mSpinner;
     private String[] arrayString;
+    private AppCompatImageView mImageView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_user_info, container, false);
-        arrayString = new String[]{"User","Ambulance","Hospital"};
-        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(view.getContext(),
-                android.R.layout.simple_spinner_item, arrayString);
+        mSpinner = (AppCompatSpinner) view.findViewById(R.id.user_spinner);
+        arrayString = new String[]{"User", "Ambulance", "Hospital"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(view.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, arrayString);
+
+        mSpinner.setAdapter(arrayAdapter);
+        mSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if (arrayString[position].equalsIgnoreCase("Ambulance")) {
+                    mImageView.setImageResource(R.drawable.ic_local_shipping_black_24dp);
+                    mImageView.setScaleType(ImageView.ScaleType.CENTER);
+                } else if (arrayString[position].equalsIgnoreCase("Hospital")) {
+                    mImageView.setImageResource(R.drawable.ic_local_hospital_black_24dp);
+                    mImageView.setScaleType(ImageView.ScaleType.CENTER);
+                } else {
+                    mImageView.setImageResource(R.drawable.ic_person_outline_black_24dp);
+                    mImageView.setScaleType(ImageView.ScaleType.CENTER);
+                }
+            }
+        });
         return view;
     }
 }
